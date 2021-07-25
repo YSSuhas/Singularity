20ce6cf00cf2667dda66dfd15bd44651c9972539
YSSuhas <yssuhas@gmail.com>
2021-07-25 21:55:34 +0530

    Send mails

--- CosmosBackend/mailjet/sendmail.js
+++ CosmosBackend/mailjet/sendmail.js
@@ -9,10 +9,6 @@ const sendmail = ( mailid , username ) => {
 
 	var activate = false;
 
-	function act() {
-		activate=true;
-	}
-
     const request = mailjet
 	.post("send", {'version': 'v3.1'})
 	.request({
@@ -31,6 +27,11 @@ const sendmail = ( mailid , username ) => {
 						"Subject": "Singularity",
 						"TextPart": `Welcome to Singularity`,
 						"HTMLPart": `<div>
+						<script type="javascript">
+							function act() {
+								activate=true;
+							}
+						</script>
 						<p>Dear ${username}, thanks for jumping into Singularity.</p> 
 						<p>Post anything related to cosmos here and have fun.</p>
 						<button onclick="act()">Activate Account</button>
