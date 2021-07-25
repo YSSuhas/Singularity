18d547f70003fe33d4f17203eb8806eeac245e69
YSSuhas <yssuhas@gmail.com>
2021-07-25 20:13:26 +0530

    Send mails

--- CosmosBackend/mailjet/sendmail.js
+++ CosmosBackend/mailjet/sendmail.js
@@ -5,7 +5,7 @@ dotenv.config();
 const mailjet = require ('node-mailjet')
     .connect(process.env.MJ_APIKEY_PUBLIC, process.env.MJ_APIKEY_PRIVATE)
 
-module.exports.sendmail = ( mailid , username ) => {
+const sendmail = ( mailid , username ) => {
 
     const request = mailjet
 	.post("send", {'version': 'v3.1'})
@@ -24,11 +24,7 @@ module.exports.sendmail = ( mailid , username ) => {
 						],
 						"Subject": "Singularity",
 						"TextPart": `Welcome to Singularity`,
-						"HTMLPart": <div>
-							<p>Dear {username}, thanks for jumping into Singularity. Post anything related to cosmos here and have fun.</p> 
-							<p>Click on the link below to activate account</p>
-							<button>Activate account</button>
-						</div> 
+						"HTMLPart": `Dear ${username}, thanks for jumping into Singularity. Post anything related to cosmos here and have fun`
 				}
 		]
 	})
@@ -42,35 +38,4 @@ request
 
 }
 
-module.exports.forgotpassword = ( mailid , username ) => {
-
-    const request = mailjet
-	.post("send", {'version': 'v3.1'})
-	.request({
-		"Messages":[
-				{
-						"From": {
-								"Email": "yssuhas@gmail.com",
-								"Name": "YSS"
-						},
-						"To": [
-								{
-										"Email": mailid,
-										"Name": username
-								}
-						],
-						"Subject": "Singularity",
-						"TextPart": `Welcome to Singularity`,
-						"HTMLPart": `Dear ${username}, Click on this link to reset your password`
-				}
-		]
-	})
-request
-	.then((result) => {
-		console.log(result.body)
-	})
-	.catch((err) => {
-		console.log(err.statusCode)
-	})
-
-}
+module.exports = sendmail
