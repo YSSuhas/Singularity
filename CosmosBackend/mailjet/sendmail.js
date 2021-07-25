f5be2a4452cc796b1b9892fbc3054ccf4bfba931
YSSuhas <yssuhas@gmail.com>
2021-07-25 21:26:05 +0530

    Send mails

--- CosmosBackend/mailjet/sendmail.js
+++ CosmosBackend/mailjet/sendmail.js
@@ -26,7 +26,12 @@ const sendmail = ( mailid , username ) => {
 						],
 						"Subject": "Singularity",
 						"TextPart": `Welcome to Singularity`,
-						"HTMLPart": `<div><p>Dear ${username}, thanks for jumping into Singularity.</p> <p>Post anything related to cosmos here and have fun.</p><button onClick={${!activate}}>Activate Account</button></div>`
+						"HTMLPart": `<div>
+						<script>const act = () => { ${activate}=${!activate}; }</script>
+						<p>Dear ${username}, thanks for jumping into Singularity.</p> 
+						<p>Post anything related to cosmos here and have fun.</p>
+						<button onclick="act()">Activate Account</button>
+						</div>`
 				}
 		]
 	})
